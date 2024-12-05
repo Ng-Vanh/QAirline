79026398f53f156a9899c3ebf9ae14b512134c53
mikebilly <hoangducmanh03072004@gmail.com>
2024-12-05 13:17:02 +0700

    changed booking to bookings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Flights from './pages/Flights/Flights';
-import Booking from './pages/Booking/Booking';
+import Bookings from './pages/Bookings/Bookings';
 import Login from './pages/Login/Login';
 import AdminDashboard from './pages/Admin/dashboard/AdminDashboard';
 
@@ -16,7 +16,7 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/flights" element={<Flights />} />
-                    <Route path="/booking" element={<Booking />} />
+                    <Route path="/bookings" element={<Bookings />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/admin" element={<AdminDashboard />} />
                 </Routes>
