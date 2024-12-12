667b0e72400f894d6ce5cce01357bc66798df48d
mikebilly <hoangducmanh03072004@gmail.com>
2024-12-12 19:15:06 +0700

    make popular flights clickable and redirect to flights page with the choosen criteria

--- frontend/src/components/popular_flight/PopularFlights.jsx
+++ frontend/src/components/popular_flight/PopularFlights.jsx
@@ -4,11 +4,14 @@ import { useState, useEffect } from 'react';
 import { Plane } from 'lucide-react';
 import styles from './PopularFlights.module.css';
 import API_BASE_URL from '~/pages/Admin/config';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function PopularFlights() {
     const [flights, setFlights] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const location = useLocation();
+    const navigate = useNavigate();
 
     useEffect(() => {
         async function fetchPopularFlights() {
@@ -29,6 +32,23 @@ export default function PopularFlights() {
         fetchPopularFlights();
     }, []);
 
+    const handleFlightClick = (departureCity, destinationCity) => {
+        const prevState = {
+            searchType: 'oneWay',
+            departureCity: departureCity || '',
+            destinationCity: destinationCity || '',
+            passengerCount: 1,
+        };
+
+        console.log("Clicked")
+        navigate('/flights', {
+          state: {
+            from: location.pathname,
+            prevState: prevState
+          },
+        });
+    }
+
     return (
         <section id="popular-flights" className={styles.popularFlights}>
             <h2>Popular Flights</h2>
@@ -41,7 +61,11 @@ export default function PopularFlights() {
             ) : (
                 <div className={styles.flightList}>
                     {flights.map((flight, index) => (
-                        <div key={index} className={styles.flightCard}>
+                        <div
+                            key={index}
+                            className={styles.flightCard}
+                            onClick={() => handleFlightClick(flight.departureCity, flight.arrivalCity)}
+                        >
                             <Plane className={styles.icon} />
                             <h3>{flight.departureCity} to {flight.arrivalCity}</h3>
                             <p>{flight.flightCount} flights</p>
